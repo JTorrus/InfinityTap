f33e700cbce59f701525cb661ffe1a6c35c84540
JTorrus <jtorrusgalindo@gmail.com>
2018-05-15 16:24:48 +0200

    Views implemented in Storyboard with their corresponding ViewControllers

--- InfinityTap/GameViewController.swift
+++ InfinityTap/GameViewController.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  GameViewController.swift
 //  InfinityTap
 //
-//  Created by Alumne on 3/5/18.
+//  Created by Alumne on 15/5/18.
 //  Copyright © 2018 Alumne. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
 
+    @IBOutlet weak var containerButton: UIView!
+    var buttonWidth: CGFloat?
+    var buttonHeight: CGFloat?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
